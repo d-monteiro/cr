39f16fc03650f21f987abc3193bce3c223512611
d-monteiro <duartemsm26@gmail.com>
2024-11-25 22:48:32 +0000

    final one

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,5 +18,8 @@ export default {
   		colors: {}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+	require("tailwindcss-animate"),
+	require('flowbite/plugin')
+  ],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "flowbite": path.resolve(__dirname, "node_modules/flowbite")
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: ['flowbite'],
+  },
   build: {
-    rollupOptions: {
-      external: ['flowbite']
-    }
-  }
+    commonjsOptions: {
+      include: [/flowbite/, /node_modules/],
+    },
+  },
 })
